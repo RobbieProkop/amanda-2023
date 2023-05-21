30f30f6c930a1f25303c86b512ee47111e9c82be
Robbie Prokop <robbieprokop@gmail.com>
2023-05-21 17:09:36 -0600

    get service cards to appear

--- src/components/ServiceDropDown.jsx
+++ src/components/ServiceDropDown.jsx
@@ -1,48 +1,11 @@
 import "../styles/serviceDropDown.scss";
 import { useEffect, useState } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import services from "../servicesArr";
 
 const ServiceDropDown = () => {
+  console.log("s", services);
   const [visible, setVisible] = useState([]);
-
-  const services = [
-    {
-      id: "1",
-      title: "Theraputic Massage",
-      value:
-        "This includes a tendency to experience intense anxiety over relatively small things, as well as difficulty with emotional closeness in relationships with others.",
-    },
-    {
-      id: "2",
-      title: "Relaxation Massage",
-      value:
-        "Panic attacks are sudden, often unexplained episodes of severe anxiety accompanied by distressing sensations such as palpitations, choking, chest pain, dizziness, diarrhea, and nausea. Panic attacks are characterized by a rapid increase in fear and typically have a short duration. They are often accompanied by various fears such as fear of death, fear of going crazy, or fear of losing control.",
-    },
-    {
-      id: "3",
-      title: "Trauma Informed Massage",
-      value:
-        "This psychological process is related to the mechanisms of psychological defense. It consists in the unconscious conviction that one is able to control everything. A natural consequence of such a conviction is a person's sense of responsibility for everything around and feelings of guilt or anxiety that arise if something gets out of control. Additionally, it can cause difficulty in fully relaxing the body and a lack of internal permission to rest, relax, and let go of control.",
-    },
-    {
-      id: "4",
-      title: "Craniosacral Therapy",
-      value:
-        "These are disorders that manifest at the physiological level and are exacerbated by stress, as well as physiological disorders that manifest at the mental level.",
-    },
-    {
-      id: "5",
-      title: "Cupping",
-      value:
-        "This includes a subjective, negative evaluation of oneself, identification of errors and shortcomings, and self-criticism.",
-    },
-    {
-      id: "6",
-      title: "Reiki",
-      value:
-        "This involves not recognizing reality as it is and not accepting oneself for one's body, character traits, and abilities. This can lead to a lack of calm and clear attention to one's capabilities and existing limitations.",
-    },
-  ];
 
   const toggleAnswer = (id) => {
     setVisible((prev) => {
---
+++ src/servicesArr.ts
@@ -0,0 +1,45 @@
+const services = [
+  {
+    id: "1",
+    title: "Theraputic Massage",
+    photo: "public/assets/yuli.jpg",
+    value:
+      "This includes a tendency to experience intense anxiety over relatively small things, as well as difficulty with emotional closeness in relationships with others.",
+  },
+  {
+    id: "2",
+    title: "Relaxation Massage",
+    photo: "public/assets/yuli.jpg",
+    value:
+      "Panic attacks are sudden, often unexplained episodes of severe anxiety accompanied by distressing sensations such as palpitations, choking, chest pain, dizziness, diarrhea, and nausea. Panic attacks are characterized by a rapid increase in fear and typically have a short duration. They are often accompanied by various fears such as fear of death, fear of going crazy, or fear of losing control.",
+  },
+  {
+    id: "3",
+    title: "Trauma Informed Massage",
+    photo: "public/assets/yuli.jpg",
+    value:
+      "This psychological process is related to the mechanisms of psychological defense. It consists in the unconscious conviction that one is able to control everything. A natural consequence of such a conviction is a person's sense of responsibility for everything around and feelings of guilt or anxiety that arise if something gets out of control. Additionally, it can cause difficulty in fully relaxing the body and a lack of internal permission to rest, relax, and let go of control.",
+  },
+  {
+    id: "4",
+    title: "Craniosacral Therapy",
+    photo: "public/assets/yuli.jpg",
+    value:
+      "These are disorders that manifest at the physiological level and are exacerbated by stress, as well as physiological disorders that manifest at the mental level.",
+  },
+  {
+    id: "5",
+    title: "Cupping",
+    photo: "public/assets/yuli.jpg",
+    value:
+      "This includes a subjective, negative evaluation of oneself, identification of errors and shortcomings, and self-criticism.",
+  },
+  {
+    id: "6",
+    title: "Reiki",
+    photo: "public/assets/yuli.jpg",
+    value:
+      "This involves not recognizing reality as it is and not accepting oneself for one's body, character traits, and abilities. This can lead to a lack of calm and clear attention to one's capabilities and existing limitations.",
+  },
+];
+export default services;
