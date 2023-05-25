6f8c15b63945f81f1ab64eb79f0628fdbf7fe65a
Robbie Prokop <robbieprokop@gmail.com>
2023-05-24 19:36:47 -0600

    add links to buttons

---
+++ src/helpers/reviews.ts
@@ -0,0 +1,52 @@
+const services = [
+  {
+    id: 1,
+    name: "Evanne",
+    value:
+      "Amanda is an amazing part of my self care routine. Her home is very comfortable and inviting. She has taken me through some painful times with care and knowledge. Highly recommend!!",
+  },
+  {
+    id: 2,
+    name: "Craniosacral Therapy",
+    value:
+      "Amanda has a very intuitive touch and her cupping skills are magic! Highly recommend for trauma recovery, or just general maintenance and relaxation",
+  },
+
+  {
+    id: 3,
+    name: "Stephanie",
+    value:
+      "Just had my very first Massage ever with Amanda, and what a Wonderful experience! Amanda was so kind and conscientious to the fact I have never really trusted anyone to do a massage before. Her space was so comfortable, warm and inviting! Thank you so much for your sensitivity and professionalism Amanda, I was able to look over my right shoulder again 😁 10 out of 10 would recommend her!",
+  },
+  {
+    id: 4,
+    name: "Phillipa",
+    value:
+      "I went to Amanda for a massage after the holidays - looking for some relaxation and attention to post-ski sore muscles/injuries. She treated me with care and attention and did a fantastic job at alleviating my pain. I left feeling refreshed and much better - I'll definitely be back!",
+  },
+  {
+    id: 5,
+    name: "Katie",
+    value:
+      "Massage was excellent and managed to be both deep tissue and relaxing at the same time. Body feels looser and happier today. Highly recommend!",
+  },
+  {
+    id: 6,
+    name: "Joseph",
+    value:
+      "Amanda is a talented, kind, and professional RMT! I had a very sore neck from a high speed tubing crash 💥 and she was able to get full mobility back in my neck! Thanks Amanda, and good luck in your new practice. I’d highly recommend booking with her for your next appointment.",
+  },
+  {
+    id: 7,
+    name: "Kate",
+    value:
+      "Amanda always listens to what I need and provides me with continued care. I appreciate Amanda’s presence, approach, and technique. Amanda is always learning and expanding her knowledge to provide the best care possible.",
+  },
+  {
+    id: 8,
+    name: "Robbie",
+    value:
+      "Best massage Therapist in town! Amanda is very sensitive to the amount of pressure I need. She always leaves my body feeling refreshed and rejuvenated. Thanks Amanda!",
+  },
+];
+export default services;
